79fc9b25fceca5675be9b6fb3765800c1587a990
A1xarT <illya030778@gmail.com>
2022-12-12 18:54:12 +0200

    client

---
+++ project3/client/src/main/java/models/Measurement.java
@@ -0,0 +1,61 @@
+package models;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Measurement {
+    private double value;
+    private boolean raining;
+    private Sensor sensor;
+
+    public Measurement(double value, boolean raining, Sensor sensor) {
+        this.value = value;
+        this.raining = raining;
+        this.sensor = sensor;
+    }
+
+    public Measurement() {
+    }
+
+    public static Measurement getRandomInstance(Sensor sensor) {
+        var random = new Random();
+        Measurement measurement = new Measurement();
+        measurement.setRaining(ThreadLocalRandom.current().nextBoolean());
+        measurement.setValue(ThreadLocalRandom.current().nextDouble(-100, 100));
+        measurement.setSensor(sensor);
+        return measurement;
+    }
+
+    @Override
+    public String toString() {
+        return "Measurement{" +
+                "value=" + value +
+                ", raining=" + raining +
+                ", sensor=" + sensor +
+                '}';
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
