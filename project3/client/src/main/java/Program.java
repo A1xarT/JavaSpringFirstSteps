d9372ad8824a6dd41b82b487248b20ca6c7e748f
A1xarT <illya030778@gmail.com>
2022-12-13 15:29:16 +0200

    add flowchart

--- project3/client/src/main/java/Program.java
+++ project3/client/src/main/java/Program.java
@@ -1,8 +1,15 @@
+import models.Measurement;
 import models.Sensor;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 import services.RestTemplateService;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Program {
     public static void main(String[] args) throws IOException {
@@ -13,9 +20,10 @@ public class Program {
         System.out.println("Measurements table");
         Scanner myInput = new Scanner(System.in);
         Sensor currentSensor = null;
+        List<Measurement> measurements = null;
         int key;
         do {
-            System.out.println("1 - random insert, 2 - show all, 3 - register new sensor, 4 - set sensor, 5 - show current sensor's name, 6 - clear, 0 - exit");
+            System.out.println("1 - random insert, 2 - show all, 3 - register new sensor, 4 - set sensor, 5 - show current sensor's name, 6 - clear, 7 - flowchart, 0 - exit");
             key = myInput.nextInt();
             switch (key) {
                 case 1 -> {
@@ -25,13 +33,14 @@ public class Program {
                     }
                     System.out.println("Input number of instances to insert:");
                     RestTemplateService.insertRandomMeasurements(myInput.nextInt(), currentSensor);
+                    measurements = getMeasurements(currentSensor);
                 }
                 case 2 -> {
                     if (currentSensor == null) {
                         System.out.println("You should set sensor first");
                         break;
                     }
-                    var measurements = RestTemplateService.findAllMeasurementsBySensor(currentSensor);
+                    measurements = RestTemplateService.findAllMeasurementsBySensor(currentSensor);
                     for (var m : measurements) {
                         System.out.println(m);
                     }
@@ -41,15 +50,18 @@ public class Program {
                     var sensor = RestTemplateService.registerSensor(myInput.next());
                     if (sensor != null) {
                         currentSensor = sensor;
+                        measurements = getMeasurements(sensor);
                     }
                 }
                 case 4 -> {
                     System.out.println("Input name of the sensor");
-                    currentSensor = RestTemplateService.findSensorByName(myInput.next());
+                    var sensor = RestTemplateService.findSensorByName(myInput.next());
+                    currentSensor = sensor;
+                    measurements = getMeasurements(sensor);
                 }
                 case 5 -> {
                     if (currentSensor == null) {
-                        System.out.println("Sensor is not set yet");
+                        System.out.println("You should set sensor first");
                     } else System.out.println("Current sensor' name: " + currentSensor.getName());
                 }
                 case 6 -> {
@@ -57,9 +69,38 @@ public class Program {
                         System.out.println("\b");
                     }
                 }
+                case 7 -> {
+                    if (measurements == null) {
+                        System.out.println("You should set sensor first");
+                    } else {
+                        List<Double> xValues = new ArrayList<>() {
+                        }, yValues = new ArrayList<>() {
+                        };
+                        for (int i = 0; i < measurements.size(); i++) {
+                            xValues.add((double) i);
+                            yValues.add(measurements.get(i).getValue());
+                        }
+                        flowChart(Stream.of(xValues.toArray(Double[]::new)).mapToDouble(Double::doubleValue).toArray(),
+                                Stream.of(yValues.toArray(Double[]::new)).mapToDouble(Double::doubleValue).toArray());
+                    }
+                }
                 default -> {
                 }
             }
         } while (key != 0);
+    }
+
+    public static void flowChart(double[] xData, double[] yData) {
+        if (xData == null || xData.length == 0 || yData == null || yData.length == 0)
+            return;
+        // Create Chart
+        XYChart chart = QuickChart.getChart("Measurements", "Measurement#", "Value", "y(x)", xData, yData);
+
+        // Show it
+        new SwingWrapper(chart).displayChart();
+    }
+
+    private static List<Measurement> getMeasurements(Sensor sensor) {
+        return RestTemplateService.findAllMeasurementsBySensor(sensor);
     }
 }
